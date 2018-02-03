33a105f56985c589f633c012843c870db7c43360
Juan neri <juanjo.neri@gmail.com>
2018-02-03 11:09:13 -0800

    implement extra methods

--- homework-1/MazeProblem.py
+++ homework-1/MazeProblem.py
@@ -44,18 +44,21 @@ For example, if an agent is at state (1, 1), and can only move right and down
 into clear tiles (.), then the transitions for that s = (1, 1) would be:
 [("R", 1, (2, 1)), ("D", 1, (1, 2))]
 '''
+import numpy as np
+import math
 class MazeProblem:
     # Static costMap for maze components and the cost to move onto them
     # Any component not listed assumed to have a cost of 1
-    costMap = {"M": 3}
-    
+    costMap = {"M": 3, ".": 1}
+    actions = {"U":(0, -1) , "D":(0, 1), "L":(-1, 0), "R":(1, 0)}
+
     # MazeProblem Constructor:
     # Constructs a new pathfinding problem from a maze, described above
     def __init__(self, maze):
         self.maze = maze
         self.initial = None
         self.goals = []
-        
+
         for r in list(enumerate(maze)):
             for c in list(enumerate(r[1])):
                 state = (c[0], r[0])
@@ -63,34 +66,56 @@ class MazeProblem:
                     self.initial = state
                 if c[1] is "G":
                     self.goals.append(state)
-    
+
     # goalTest is parameterized by a state, and
     # returns True if the given state is a goal, False otherwise
     def goalTest(self, state):
-        # TODO: Implement as intended
-        return False
-    
+        try:
+            col, row = state
+            return self.maze[row][col] == 'G'
+        except:
+            return False
+
+    @staticmethod
+    def _distance(initial_state, final_state):
+        return np.linalg.norm(np.array(initial_state)-np.array(final_state))
+
+    def valid(self, state):
+        col, row = state
+        try:
+            return self.maze[row][col] != 'X'
+        except:
+            return False
+
     # Implements the Manhattan Distance Heuristic, which (given a state)
     # provides the cell-distance to the nearest goal state
     def heuristic(self, state):
-        # TODO: Implement as intended
-        return 0
-    
+        min_distance = math.inf
+        for goal in self.goals:
+            min_distance = min(min_distance, _distance(state, goal))
+        return min_distance
+
     # transitions returns a list of tuples in the format:
     # [(action1, cost_of_action1, result(action1, s), ...]
     # corresponding to allowable actions of the given state, as well
     # as the next state the action leads to
     def transitions(self, state):
-        # TODO: Implement as intended
-        return []
-    
+        col, row = state
+        transitions = []
+        for action, delta in MazeProblem.actions.items():
+            d_col, d_row = delta
+            result = (col+d_col, row+d_row)
+            if self.valid(result):
+                transitions.append((action, self.cost(result), result))
+        return transitions
+
     # cost returns the cost of moving onto the given state, and employs
     # the MazeProblem's costMap
     def cost(self, state):
         cm = MazeProblem.costMap
         cell = self.maze[state[1]][state[0]]
-        return cm[cell] if cell in cm else 1
-    
+        return cm[cell]
+
     # solnTest will return a tuple of the format (cost, isSoln) where:
     # cost = the total cost of the solution,
     # isSoln = true if the given sequence of actions of the format:
@@ -106,4 +131,3 @@ class MazeProblem:
             if self.maze[s[1]][s[0]] == "X":
                 return (-1, False)
         return (tc, self.goalTest(s))
-    
